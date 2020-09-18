6afdb91738d6e22393059949bde4e400e5938e59
Fjorde <71193653+Fjorde@users.noreply.github.com>
2020-09-18 22:22:43 +0200

    added km - m/m - km and km - yd/yd - km conversion

--- Measurement Converter.py
+++ Measurement Converter.py
@@ -9,15 +9,15 @@ value = float(input("Value: "))
 #Messages
 invalid = "What you have typed is invalid. Please type two valid measurements"
 valid = "Your measurements have been received. Calculating..."
-#Temperature
+#Length
 if unittype == "L":
-    if unit1 == "cm" and unit2 == "m":
+	if unit1 == "cm" and unit2 == "m":
         print(valid)
         ans = float(value/100)
-    elif unit1 == "m" and unit2 == "cm":
+	elif unit1 == "m" and unit2 == "cm":
         print(valid)
         ans = float(value*100)
-    elif unit1 == "ft" and unit2 == "m":
+	elif unit1 == "ft" and unit2 == "m":
         print(valid)
         ans = float((value/100)*30.48)
 	elif unit1 == "ft" and unit2 == "in":
@@ -38,15 +38,28 @@ if unittype == "L":
 	elif unit1 == "yd" and unit2 == "in":
 		print(valid)
 		ans = float(value*36)
+	elif unit1 == "km" and unit2 == "m":
+		print(valid)
+		ans = float(value*1000)
+	elif unit1 == "m" and unit2 == "km":
+		print(valid)
+		ans = float(value/1000)
+	elif unit1 == "km" and unit2 == "yd":
+		print(valid)
+		ans = float(value*1093.61)
+	elif unit1 == "yd" and unit2 == "km":
+		print(valid)
+		ans = float(value/1093.61)
     else:
         print(invalid)
     print(ans, unit2)
 #Temperature
+#Finished
 if unittype == "T":
-    if unit1 == "f" and unit2 == "c":
+	if unit1 == "f" and unit2 == "c":
         print(valid)
         ans = float((value-32)*(5/9))
-    elif unit1 == "k" and unit2 == "c":
+	elif unit1 == "k" and unit2 == "c":
         print(valid)
         ans = float(value+273.15)
     elif unit1 == "f" and unit2 == "k":
